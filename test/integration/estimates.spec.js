49df9369c3cf390a72ae9d2e5d0a05808a2373d9
Thiago Araujo <thd.araujo@gmail.com>
2020-08-19 16:23:07 -0700

    specs for project and metadata

--- test/integration/estimates.spec.js
+++ test/integration/estimates.spec.js
@@ -19,4 +19,18 @@ describe('Estimates Integration', function () {
     })
     expect(retrieveEstimatesResponse.data.length).to.be.above(0)
   })
+
+  it('supports create with a specific project', async function () {
+    const { data } = await patch.projects.retrieveProjects()
+    const projectId = data[0].id
+
+    const createEstimateResponse = await patch.estimates.createMassEstimate({
+      mass_g: 100,
+      project_id: projectId
+    })
+
+    expect(createEstimateResponse.success).to.equal(true)
+    expect(createEstimateResponse.data.id).not.to.be.empty
+    expect(createEstimateResponse.data.order.mass_g).to.eq(100)
+  })
 })
--- test/integration/orders.test.js
+++ test/integration/orders.test.js
@@ -7,13 +7,40 @@ describe('Orders Integration', function () {
     const createOrderResponse = await patch.orders.createOrder({ mass_g: 100 })
     const orderId = createOrderResponse.data.id
 
-    const retreiveOrderResponse = await patch.orders.retrieveOrder(orderId)
-    expect(retreiveOrderResponse.data.id).to.equal(orderId)
+    const retrieveOrderResponse = await patch.orders.retrieveOrder(orderId)
+    expect(retrieveOrderResponse.data.id).to.equal(orderId)
 
     const retrieveOrdersResponse = await patch.orders.retrieveOrders({
       page: 1
     })
     expect(retrieveOrdersResponse.data.length).to.be.above(0)
+  })
+
+  it('supports create with a specific project', async function () {
+    const { data } = await patch.projects.retrieveProjects()
+    const projectId = data[0].id
+
+    const createOrderResponse = await patch.orders.createOrder({
+      mass_g: 100,
+      project_id: projectId
+    })
+
+    expect(createOrderResponse.success).to.equal(true)
+    expect(createOrderResponse.data.id).not.to.be.empty
+  })
+
+  it('supports create with metadata', async function () {
+    const metadata = { user: 'john doe' }
+
+    const createOrderResponse = await patch.orders.createOrder({
+      mass_g: 100,
+      metadata: metadata
+    })
+
+    expect(createOrderResponse.success).to.equal(true)
+    expect(createOrderResponse.data.id).not.to.be.empty
+    // console.log(createOrderResponse.data)
+    // expect(createOrderResponse.data.metadata).to.eq(metadata)
   })
 
   it('supports placing orders in a `draft` state', async function () {
