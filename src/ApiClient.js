62a3a71e9a5ce8e86f29d4c31ce67f6e2c7f71f9
Lovisa Svallingson <lovisa@usepatch.com>
2020-09-18 18:25:27 -0600

    v1.2.1 (#10)

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -411,7 +411,7 @@ class ApiClient {
   hostSettings() {
     return [
       {
-        url: 'https://{api.usepatch.com}',
+        url: 'https://api.usepatch.com',
         description: 'No description provided',
 
         variables: {
