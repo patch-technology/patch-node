49e127ed524e94b6223d332bb39c83ada6cb61c0
Thiago Araujo <thd.araujo@gmail.com>
2020-10-14 15:24:55 -0700

    order price creation (#16)

--- test/integration/orders.test.js
+++ test/integration/orders.test.js
@@ -1,19 +1,31 @@
 import { expect } from 'chai';
 import Patch from '../../dist/index';
 const patch = Patch(process.env.SANDBOX_API_KEY);
+const biomass_test_project_id = 'pro_test_c3a9feba769fc7a8806377266ca9ff6a';
 
 describe('Orders Integration', function () {
   it('supports create, place, cancel, retrieve and list', async function () {
     const createOrderResponse = await patch.orders.createOrder({ mass_g: 100 });
     const orderId = createOrderResponse.data.id;
 
-    const retreiveOrderResponse = await patch.orders.retrieveOrder(orderId);
-    expect(retreiveOrderResponse.data.id).to.equal(orderId);
+    const retrieveOrderResponse = await patch.orders.retrieveOrder(orderId);
+    expect(retrieveOrderResponse.data.id).to.equal(orderId);
 
     const retrieveOrdersResponse = await patch.orders.retrieveOrders({
       page: 1
     });
     expect(retrieveOrdersResponse.data.length).to.be.above(0);
+  });
+
+  it('supports create orders with a total price', async function () {
+    const createOrderResponse = await patch.orders.createOrder({
+      total_price_cents_usd: 500,
+      project_id: biomass_test_project_id
+    });
+
+    expect(createOrderResponse.data.price_cents_usd).to.equal('500.0');
+    expect(createOrderResponse.data.patch_fee_cents_usd).to.equal('0.0');
+    expect(createOrderResponse.data.mass_g).to.equal(5000000);
   });
 
   it('supports placing orders in a `draft` state', async function () {
