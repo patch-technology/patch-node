ed8dc781eccefab1fe2a74debf5bfeba98a81d92
Lovisa Svallingson <lovisasvallingson@gmail.com>
2021-02-09 09:04:22 -0700

    refactor preferences test

--- test/integration/preferences.test.js
+++ test/integration/preferences.test.js
@@ -3,45 +3,45 @@ import Patch from '../../dist/index';
 const patch = Patch(process.env.SANDBOX_API_KEY);
 
 describe('Preferences Integration', async function () {
-  it('supports create, delete, retrieve and list', async function () {
-    const projectResponse = await patch.projects.retrieveProjects();
-    expect(projectResponse.data.length).to.be.above(0);
+  it('supports creating, deleting, and listing preferences', async function () {
+    const preferencesList = await patch.preferences.retrievePreferences();
 
-    const projectId = projectResponse.data[0].id;
-    let createPreferenceResponse;
+    // If there is an existing preference, delete it and create a new one with the same
+    // project id. If there is not any existing preferences, create and delete one with
+    // any project id.
+    if (preferencesList.data.length > 0) {
+      const preference = preferencesList.data[0];
+      const projectId = preference.project.id;
 
-    try {
-      createPreferenceResponse = await patch.preferences.createPreference({
+      await patch.preferences.deletePreference(preference.id);
+      const createdPreference = await patch.preferences.createPreference({
         project_id: projectId
       });
-    } catch ({ error }) {
-      // If there's a preference that exists already
-      if (error.code === 422) {
-        // Delete the preference
-        await patch.preferences.deletePreference(
-          (await patch.preferences.retrievePreferences()).data[0].id
-        );
 
-        createPreferenceResponse = await patch.preferences.createPreference({
-          project_id: projectId
-        });
-      }
+      expect(createdPreference.data.project.id).to.eq(projectId);
+
+      const preferencesList2 = await patch.preferences.retrievePreferences();
+      expect(preferencesList2.data.length).to.be.above(0);
+    } else {
+      const projectResponse = await patch.projects.retrieveProjects();
+      expect(projectResponse.data.length).to.be.above(0);
+      const projectId = projectResponse.data[0].id;
+
+      const createdPreference = await patch.preferences.createPreference({
+        project_id: projectId
+      });
+      console.log(createdPreference);
+
+      expect(createdPreference.data.projectId).to.eq(projectId);
+
+      const preferencesList2 = await patch.preferences.retrievePreferences();
+      const preferencesCount = preferencesList2.data.length;
+      expect(preferencesList2.data.length).to.be.above(0);
+
+      await patch.preferences.deletePreference(preference.id);
+
+      const preferencesList3 = await patch.preferences.retrievePreferences();
+      expect(preferencesList3.data.length).to.eq(preferencesCount - 1);
     }
-
-    const preferenceId = createPreferenceResponse.data.id;
-    const retrievePreferenceResponse = await patch.preferences.retrievePreference(
-      preferenceId
-    );
-    expect(retrievePreferenceResponse.data.id).to.equal(preferenceId);
-
-    const retrievePreferencesResponse = await patch.preferences.retrievePreferences(
-      { page: 1 }
-    );
-    expect(retrievePreferencesResponse.data[0].id).to.equal(preferenceId);
-
-    const deletePreferenceResponse = await patch.preferences.deletePreference(
-      preferenceId
-    );
-    expect(deletePreferenceResponse.data.id).to.equal(preferenceId);
   });
 });
