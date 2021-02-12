3f8de13ef311c9d6a290f3b969e8c5a8a42d697a
Thiago Araujo <thd.araujo@gmail.com>
2021-02-11 16:32:08 -0800

    adds sdgs, makes some vehicle estimate fields optional

---
+++ src/model/Sdg.js
@@ -0,0 +1,57 @@
+/**
+ * Patch API V1
+ * The core API used to integrate with Patch's service
+ *
+ * Contact: developers@usepatch.com
+ */
+
+import ApiClient from '../ApiClient';
+
+class Sdg {
+  constructor(title, _number, description, url) {
+    Sdg.initialize(this, title, _number, description, url);
+  }
+
+  static initialize(obj, title, _number, description, url) {
+    obj['title'] = title;
+    obj['number'] = _number;
+    obj['description'] = description;
+    obj['url'] = url;
+  }
+
+  static constructFromObject(data, obj) {
+    if (data) {
+      obj = obj || new Sdg();
+
+      if (data.hasOwnProperty('title')) {
+        obj['title'] = ApiClient.convertToType(data['title'], 'String');
+      }
+
+      if (data.hasOwnProperty('number')) {
+        obj['number'] = ApiClient.convertToType(data['number'], 'Number');
+      }
+
+      if (data.hasOwnProperty('description')) {
+        obj['description'] = ApiClient.convertToType(
+          data['description'],
+          'String'
+        );
+      }
+
+      if (data.hasOwnProperty('url')) {
+        obj['url'] = ApiClient.convertToType(data['url'], 'String');
+      }
+    }
+    return obj;
+  }
+}
+
+Sdg.prototype['title'] = undefined;
+
+Sdg.prototype['number'] = undefined;
+
+Sdg.prototype['description'] = undefined;
+
+Sdg.prototype['url'] = undefined;
+
+export default Sdg;
